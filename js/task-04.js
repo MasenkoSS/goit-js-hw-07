bc7d443ba51c6d75c2ed855eb614d44cbb4d3560
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-09-19 17:54:12 +0300

    Update task-04 переменные

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+
+const counterRef = document.querySelector('#counter');
+const counterValRef = document.querySelector('#value');
+const decrementbtnRef = document.querySelector('[data-action="decrement"]');
+const incrementbtnRef = document.querySelector('[data-action="increment"]');
+
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+
+let counterVal = 0;
+
+
+  }
+
+
+
+
+
+
+
+
