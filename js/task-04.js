611e78f3858723727d54e93600e66d6e63917c89
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-09-19 18:44:27 +0300

    Update task-04 final

--- js/task-04.js
+++ js/task-04.js
@@ -10,7 +10,20 @@ const incrementbtnRef = document.querySelector('[data-action="increment"]');
 
 let counterVal = 0;
 
+decrementbtnRef.addEventListener('click', decrement);
+incrementbtnRef.addEventListener('click', increment);
 
+
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+function decrement() {
+    counterVal -= 1;
+    counterValRef.textContent = counterVal;
+  }
+
+function increment() {
+    counterVal += 1;
+    counterValRef.textContent = counterVal;
   }
 
 
