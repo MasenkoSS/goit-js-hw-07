ac98da296238630505fa0f3214478452bf2e8c31
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-09-20 14:00:39 +0300

    Update task-06 final

--- js/task-06.js
+++ js/task-06.js
@@ -5,3 +5,16 @@ console.log(inputString);
 
 
 inputString.addEventListener('blur', onInputBlur);
+
+// Функция проверки (event.currentTarget используется, 
+// когда один и тот же обработчик события присваивается нескольким элементам.)
+
+function onInputBlur(event) {
+    if (event.currentTarget.value.length === Number(inputString.dataset.length)) {
+        inputString.classList.add('valid');
+        inputString.classList.remove('invalid');
+    }
+    else {
+        inputString.classList.add('invalid');
+    }
+}
